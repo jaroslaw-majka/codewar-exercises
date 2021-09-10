92ce9c5781e418ff5e2dfd503d702a1d1220edb7
Jarek Majka <vessekheal@gmail.com>
2021-09-10 13:06:13 +0200

    Camel case exercise initial commit

---
+++ camelCasing.py
@@ -0,0 +1,9 @@
+# Complete the solution so that the function will break up camel casing, using a space between words.
+
+# Link: https://www.codewars.com/kata/5208f99aee097e6552000148/python
+def camel_case_breaker(text):
+
+
+
+if __name__ == '__main__':
+    print(camel_case_breaker('helloWorld'))
