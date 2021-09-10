fe0339feaf5c5674af2b62870929868ed257f5cb
Jarek Majka <vessekheal@gmail.com>
2021-09-10 14:40:32 +0200

    Camel case exercise solved

--- camelCasing.py
+++ camelCasing.py
@@ -2,8 +2,14 @@
 
 # Link: https://www.codewars.com/kata/5208f99aee097e6552000148/python
 def camel_case_breaker(text):
-
+    final_string =''
+    for letter in text:
+        if letter.isupper():
+            final_string += ' ' + letter
+        else:
+            final_string += letter
+    return final_string
 
 
 if __name__ == '__main__':
-    print(camel_case_breaker('helloWorld'))
+    print(camel_case_breaker('helloWorldAndBye'))
