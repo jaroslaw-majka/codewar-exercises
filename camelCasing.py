adcbbc89df538c94cfd62d35f5b3b88895a2d489
Jarek Majka <vessekheal@gmail.com>
2021-09-10 14:50:10 +0200

    minor fixes

--- camelCasing.py
+++ camelCasing.py
@@ -2,7 +2,7 @@
 
 # Link: https://www.codewars.com/kata/5208f99aee097e6552000148/python
 def camel_case_breaker(text):
-    final_string =''
+    final_string = ''
     for letter in text:
         if letter.isupper():
             final_string += ' ' + letter
